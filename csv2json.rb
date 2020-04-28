4e68c6ce0281410adbc113588c6e3c2da2abfea7
Joshua Peek <josh@joshpeek.com>
2020-04-28 09:42:49 -0700

    Update csv2json

--- csv2json.rb
+++ csv2json.rb
@@ -1,9 +1,9 @@
 class Csv2json < Formula
   desc "A humble CSV tool, friend of jq"
   homepage "https://github.com/josh/csv2json"
-  version "0.1.0"
+  version "0.2.0"
   url "https://github.com/josh/csv2json/archive/v#{version}.tar.gz"
-  sha256 "21ebb2365e5b2ecf4e02b7b7b3fc76f08e511202e880eaf05e1bf1f229a6a69d"
+  sha256 "0c0e8864125839a399b464f11ecc0e04746558985e980f7df266191a62bf198a"
 
   def install
     system "swift", "build", "-c", "release"
