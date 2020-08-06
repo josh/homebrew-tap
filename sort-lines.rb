810893947789fb96592d50d6ae144a0d769b8d3e
Joshua Peek <josh@joshpeek.com>
2020-08-05 19:25:43 -0700

    Add sort lines service

---
+++ sort-lines.rb
@@ -0,0 +1,9 @@
+cask "sort-lines" do
+  version "a87b15e411cbe6ed4c07431df7a4feb42e07d7ec"
+  sha256 "b9034bff1122b3ccf80b3dbdd05e0c1bfae8ebf2382f555c64741169a5570d27"
+
+  url "https://github.com/josh/homebrew-tap/archive/#{version}.tar.gz"
+  name "Sort Lines"
+
+  service "homebrew-tap-#{version}/Sort Lines.workflow"
+end
