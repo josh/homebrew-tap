ea1822df102bd6c805ac20e080007a6bf27ac416
Joshua Peek <josh@joshpeek.com>
2020-08-16 23:19:21 -0700

    Place the first line of a multi-line method definition's body on its own line

--- displayrcd.rb
+++ displayrcd.rb
@@ -12,27 +12,28 @@ class Displayrcd < Formula
 
   plist_options manual: "displayrcd"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/displayrcd</string>
-        </array>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/displayrcd.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/displayrcd.log</string>
-        <key>KeepAlive</key>
-        <true/>
-        <key>RunAtLoad</key>
-        <true/>
-      </dict>
-    </plist>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/displayrcd</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/displayrcd.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/displayrcd.log</string>
+          <key>KeepAlive</key>
+          <true/>
+          <key>RunAtLoad</key>
+          <true/>
+        </dict>
+      </plist>
     EOS
   end
 
