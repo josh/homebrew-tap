55499d3b1468603e34cf367f2b9d8675d9245348
Joshua Peek <josh@joshpeek.com>
2020-08-16 22:38:16 -0700

    Merge version and url lines

--- csv2json.rb
+++ csv2json.rb
@@ -1,8 +1,7 @@
 class Csv2json < Formula
   desc "Humble CSV tool, friend of jq"
   homepage "https://github.com/josh/csv2json"
-  version "1.0.0"
-  url "https://github.com/josh/csv2json/archive/v#{version}.tar.gz"
+  url "https://github.com/josh/csv2json/archive/v1.0.0.tar.gz"
   sha256 "08709cdbcbf06918b14781c72d945c4828a5de02f60d7acc12d292d1d01dffcb"
 
   def install
