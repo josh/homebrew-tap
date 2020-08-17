7296df2e84f412e5c9959d438bea49ec109b757e
Joshua Peek <josh@joshpeek.com>
2020-08-17 00:44:14 -0700

    Formula should have a homepage

--- swift-completions.rb
+++ swift-completions.rb
@@ -1,6 +1,6 @@
 class SwiftCompletions < Formula
   desc "Swift shell completions"
-  url "https://blank.org"
+  url "http://example.com/"
   version "0.1.0"
   sha256 "69917e3a76d270be4e4f07ce97f008b3a48e9cf5cc2283e44c7f69797a21918f"
 
