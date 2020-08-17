22ee1042592f118f3ffb04c32fb7bfe52ed5ecee
Joshua Peek <josh@joshpeek.com>
2020-08-16 22:49:40 -0700

    Place the first line of a multi-line method definition's body on its own line

--- brew-unattended-upgrade.rb
+++ brew-unattended-upgrade.rb
@@ -16,33 +16,34 @@ class BrewUnattendedUpgrade < Formula
 
   plist_options manual: "brew unattended-upgrade"
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{HOMEBREW_BREW_FILE}</string>
-          <string>unattended-upgrade</string>
-        </array>
-        <key>EnvironmentVariables</key>
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
         <dict>
-          <key>PATH</key>
-          <string>/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{HOMEBREW_BREW_FILE}</string>
+            <string>unattended-upgrade</string>
+          </array>
+          <key>EnvironmentVariables</key>
+          <dict>
+            <key>PATH</key>
+            <string>/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin</string>
+          </dict>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/brew-unattended-upgrade.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/brew-unattended-upgrade.log</string>
+          <key>StartInterval</key>
+          <integer>3600</integer>
         </dict>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/brew-unattended-upgrade.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/brew-unattended-upgrade.log</string>
-        <key>StartInterval</key>
-        <integer>3600</integer>
-      </dict>
-    </plist>
+      </plist>
     EOS
   end
 end
