35326c48a7def5a7e3298c498a38a9030467df64
Joshua Peek <josh@joshpeek.com>
2021-03-11 22:14:38 -0800

    Bump

--- ipfs-tools.rb
+++ ipfs-tools.rb
@@ -5,16 +5,16 @@
 class IpfsTools < Formula
   desc "Scripts for adding and fetching IPFS content"
   homepage "https://ipfs.io/ipns/k51qzi5uqu5dgvnd5164dpo8hh4lyjuyk7i113sx2h2y7ski7i7nq4invwn6ig"
-  url "http://127.0.0.1:8080/ipfs/QmUtR86WPQrZ2cYFJLY8dPXcTj3gmzK8TPGF84erGg98DX"
-  mirror "https://ipfs.io/ipfs/QmUtR86WPQrZ2cYFJLY8dPXcTj3gmzK8TPGF84erGg98DX"
-  version "53"
-  sha256 "cb3a477dd8df2ee404265524673b7556eb0abcd3c0873ad9b039379ac04a8362"
+  url "http://127.0.0.1:8080/ipfs/QmXwuifu29XUfWN43b1UpX77RrWz4j2seBz5SZiwbJVkSp"
+  mirror "https://ipfs.io/ipfs/QmXwuifu29XUfWN43b1UpX77RrWz4j2seBz5SZiwbJVkSp"
+  version "54"
+  sha256 "1afdbd0eac849c8a3a77a42104cf567b3a09ad51baef82c89e722f384745e438"
   license "MIT"
   head "http://127.0.0.1:8080/ipns/k51qzi5uqu5dgvnd5164dpo8hh4lyjuyk7i113sx2h2y7ski7i7nq4invwn6ig",
     using: :git, branch: "main"
 
   depends_on "gnu-tar"
-  depends_on "ipfs"
+  #depends_on "ipfs"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
