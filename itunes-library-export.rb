f5d23664f03a6e73c54b1128718032bfa41b7c4d
Joshua Peek <josh@joshpeek.com>
2021-06-09 15:58:45 -0700

    Bump formula

--- itunes-library-export.rb
+++ itunes-library-export.rb
@@ -5,8 +5,8 @@
 class ItunesLibraryExport < Formula
   desc "A command line tool to export iTunes Library XML files"
   homepage "https://github.com/josh/itunes-library-export"
-  url "https://github.com/josh/itunes-library-export/archive/v0.1.0.tar.gz"
-  sha256 "bacf565fbbaa3686ae9c28f6d59166606fd410f30f6ae42e560b28ebd4d7f342"
+  url "https://github.com/josh/itunes-library-export/archive/v0.1.1.tar.gz"
+  sha256 "9323edcbb0683e45ba4b2b0d4be423ddb3fd903c62535d3c83f9de9ae11b2756"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
