4dd5731955cdf5d98b8cfcf7057037390a75580c
Joshua Peek <josh@joshpeek.com>
2021-07-06 13:52:52 -0700

    bump

--- ipfs-tools.rb
+++ ipfs-tools.rb
@@ -5,10 +5,10 @@
 class IpfsTools < Formula
   desc "Scripts for adding and fetching IPFS content"
   homepage "https://ipfs.io/ipns/k51qzi5uqu5dgvnd5164dpo8hh4lyjuyk7i113sx2h2y7ski7i7nq4invwn6ig"
-  url "http://127.0.0.1:8080/ipfs/QmQxz2NeCh6RJNJ7Vneaqd9A6uUFWtsvdiXKek3WVMH5QY"
-  mirror "https://ipfs.io/ipfs/QmQxz2NeCh6RJNJ7Vneaqd9A6uUFWtsvdiXKek3WVMH5QY"
-  version "72"
-  sha256 "9b1fba57efb64e6a9348da650298d2d125c8592ac5867d791c2a4fd3c046ee65"
+  url "http://127.0.0.1:8080/ipfs/QmY6m25fvQN9F98wRnqT8V9TjVNUeQHZVxtqv77k2Jh2YB"
+  mirror "https://ipfs.io/ipfs/QmY6m25fvQN9F98wRnqT8V9TjVNUeQHZVxtqv77k2Jh2YB"
+  version "73"
+  sha256 "2de639d198a462c7f9f5a600ca8eb9d8109dcf714412b0f5cc3c51482e9759b0"
   license "MIT"
   head "http://127.0.0.1:8080/ipns/k51qzi5uqu5dgvnd5164dpo8hh4lyjuyk7i113sx2h2y7ski7i7nq4invwn6ig",
     using: :git, branch: "main"
