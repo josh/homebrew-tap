21dbbd82ada020056028e318b5894d1e7ffb0b09
Joshua Peek <josh@users.noreply.github.com>
2023-02-06 16:54:21 -0800

    Remove plist

--- itunes-library-export.rb
+++ itunes-library-export.rb
@@ -14,35 +14,6 @@ class ItunesLibraryExport < Formula
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/itunes-library-export"
   end
-  
-  plist_options manual: "itunes-library-export"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_bin}/itunes-library-export</string>
-            <string>--output</string>
-            <string>#{ENV["HOME"]}/Music/Music/Music Library.xml</string>
-          </array>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/itunes-library-export.log</string>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/itunes-library-export.log</string>
-          <key>StartInterval</key>
-          <integer>43200</integer>
-          <key>RunAtLoad</key>
-          <true/>
-        </dict>
-      </plist>
-    EOS
-  end
 
   test do
     system bin/"itunes-library-export", "--help"
