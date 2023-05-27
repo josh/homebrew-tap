fba5d088d74c987bb96706a7b1add3fb7433a4a1
Joshua Peek <josh@joshpeek.com>
2023-05-26 17:54:28 -0700

    Map to xcode version

--- swift-completions.rb
+++ swift-completions.rb
@@ -2,7 +2,7 @@ class SwiftCompletions < Formula
   desc "Swift shell completions"
   homepage "https://swift.org/"
   url "https://github.com/josh/homebrew-tap/raw/4b9c17f996fd897382ad812a472a551d116f7172/empty.tar"
-  version "0.1.0"
+  version MacOS::Xcode.version || "0.1.0"
   sha256 "9bfac43f415467832a8470a10da79435da99c950cd20b7fae458eb2144f0ff7a"
 
   livecheck do
@@ -20,6 +20,6 @@ class SwiftCompletions < Formula
   end
 
   test do
-    42
+    system "swift", "package", "completion-tool", "--help"
   end
 end
