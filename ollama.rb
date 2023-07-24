21dcd7bac0e98475db5f1c2c8e0773a442df2ab0
Joshua Peek <josh@joshpeek.com>
2023-07-24 10:33:40 -0700

    ollama 0.0.11

--- ollama.rb
+++ ollama.rb
@@ -1,8 +1,8 @@
 class Ollama < Formula
   desc "Get up and running with large language models locally"
   homepage "https://ollama.ai"
-  url "https://github.com/jmorganca/ollama/releases/download/v0.0.10/ollama-darwin-arm64"
-  sha256 "cab5d08766b66d278618811f668040aca03f022d7b78b72928ca18447b4dfb3d"
+  url "https://github.com/jmorganca/ollama/releases/download/v0.0.11/ollama-darwin-arm64"
+  sha256 "ee86d34ca1479104382c673047709572437cddcfd2a3b37358177bdc513007c6"
   license "MIT"
 
   service do
