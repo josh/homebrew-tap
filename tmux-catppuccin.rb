77a9fc04c56ec91bfb55cea220f43f0ad153deeb
Joshua Peek <josh@joshpeek.com>
2024-09-02 13:43:47 -0700

    Add tmux theme plugins

---
+++ tmux-catppuccin.rb
@@ -0,0 +1,24 @@
+class TmuxCatppuccin < Formula
+  desc "Soothing pastel theme for Tmux!"
+  homepage "https://github.com/catppuccin/tmux"
+  url "https://github.com/catppuccin/tmux/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "f3ac9a6e9a39de4d609028e8e6183214da19559fe5ed20e6ff928745c6a97f08"
+  license "MIT"
+
+  depends_on "tmux"
+
+  def install
+    pkgshare.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      Add the following to your tmux.conf:
+        run-shell '#{opt_pkgshare}/catppuccin.tmux'
+    EOS
+  end
+
+  test do
+    assert_empty shell_output(pkgshare / "catppuccin.tmux")
+  end
+end
---
+++ tmux-tokyo-night.rb
@@ -0,0 +1,23 @@
+class TmuxTokyoNight < Formula
+  desc "Tokyo Night tmux theme directly inspired from Tokyo Night vim theme"
+  homepage "https://github.com/fabioluciano/tmux-tokyo-night"
+  url "https://github.com/fabioluciano/tmux-tokyo-night/archive/refs/tags/v1.7.0.tar.gz"
+  sha256 "c05a43997e877f2fee6f11de331658b9cd2724f1f46f1fa95a2158665583730f"
+
+  depends_on "tmux"
+
+  def install
+    pkgshare.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      Add the following to your tmux.conf:
+        run-shell '#{opt_pkgshare}/tmux-tokyo-night.tmux'
+    EOS
+  end
+
+  test do
+    assert_empty shell_output(pkgshare / "tmux-tokyo-night.tmux")
+  end
+end
