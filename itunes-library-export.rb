3580257799712c577dbe1d51df8219b47e57494c
Joshua Peek <josh@joshpeek.com>
2024-09-03 11:22:26 -0700

    Update url

--- itunes-library-export.rb
+++ itunes-library-export.rb
@@ -1,7 +1,7 @@
 class ItunesLibraryExport < Formula
   desc "Command-line tool to export iTunes Library XML files"
   homepage "https://github.com/josh/itunes-library-export"
-  url "https://github.com/josh/itunes-library-export/archive/v0.1.1.tar.gz"
+  url "https://github.com/josh/itunes-library-export/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "9323edcbb0683e45ba4b2b0d4be423ddb3fd903c62535d3c83f9de9ae11b2756"
 
   depends_on xcode: ["12.5", :build]
