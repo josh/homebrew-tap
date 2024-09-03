ef6ab15c6581aaf3ac41a2a3f85f518fff029f90
Joshua Peek <josh@joshpeek.com>
2024-09-03 12:01:13 -0700

    Add tokyonight extras

---
+++ tokyonight-extras.rb
@@ -0,0 +1,23 @@
+class TokyonightExtras < Formula
+  desc "Provides TokyoNight extras for numerous other applications"
+  homepage "https://github.com/folke/tokyonight.nvim"
+  url "https://github.com/folke/tokyonight.nvim/archive/refs/tags/v4.8.0.tar.gz"
+  sha256 "a84d4d55ab8bc4529b3599dccb3af31d3657b5a85169ff3f03278736f3694bea"
+  license "Apache-2.0"
+
+  def install
+    pkgshare.install Dir["extras/*"]
+  end
+
+  def caveats
+    <<~EOS
+      ls '#{opt_pkgshare}/'
+    EOS
+  end
+
+  test do
+    assert (pkgshare / "alacritty").exist?
+    assert (pkgshare / "fzf").exist?
+    assert (pkgshare / "tmux").exist?
+  end
+end
