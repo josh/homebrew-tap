cfe7c99ad772e646a7f0f04562184faa04c0fd06
Joshua Peek <josh@joshpeek.com>
2024-09-03 11:21:23 -0700

    Fix style

--- cmd/brew-unattended-upgrade.rb
+++ cmd/brew-unattended-upgrade.rb
@@ -1,5 +1,9 @@
-MAS = which("mas")
-NOTIFER = which("terminal-notifier")
+# frozen_string_literal: true
+
+require "English"
+
+MAS = which("mas").freeze
+NOTIFER = which("terminal-notifier").freeze
 bundle_file = ENV["HOMEBREW_BUNDLE_FILE"] || "#{HOMEBREW_PREFIX}/etc/Brewfile"
 bundle_file = nil if bundle_file && !File.exist?(bundle_file)
 
@@ -12,31 +16,33 @@ def notify(group:, message: nil, subtitle: nil, execute: nil, terminal: nil)
     "-appIcon", "https://brew.sh/assets/img/homebrew-256x256.png"
   ]
 
-  execute = Shellwords.join([
-    "osascript",
-    '-e', 'on run argv',
-    '-e', 'tell app "Terminal"',
-    '-e', 'do script (item 1 of argv)',
-    '-e', 'end tell',
-    '-e', 'end run',
-    Shellwords.join(terminal)
-  ]) if terminal
-
-  if message
-    cmd.concat ["-group", "brew-unattended-upgrades #{group}"]
-    cmd.concat ["-message", message]
-  else
-    cmd.concat ["-remove", "brew-unattended-upgrades #{group}"]
+  if terminal
+    execute = Shellwords.join([
+      "osascript",
+      "-e", "on run argv",
+      "-e", 'tell app "Terminal"',
+      "-e", "do script (item 1 of argv)",
+      "-e", "end tell",
+      "-e", "end run",
+      Shellwords.join(terminal)
+    ])
   end
 
-  cmd.concat ["-subtitle", subtitle] if subtitle
-  cmd.concat ["-execute", execute] if execute
+  if message
+    cmd.push "-group", "brew-unattended-upgrades #{group}"
+    cmd.push "-message", message
+  else
+    cmd.push "-remove", "brew-unattended-upgrades #{group}"
+  end
+
+  cmd.push "-subtitle", subtitle if subtitle
+  cmd.push "-execute", execute if execute
 
   safe_system(*cmd)
 end
 
 at_exit do
-  if $!.success?
+  if $ERROR_INFO.success?
     notify group: :exitstatus
   else
     notify group: :exitstatus, subtitle: "brew unattended-upgrade", message: "Failed"
@@ -48,29 +54,28 @@ def run!(*args, label:)
   safe_system(*args)
 end
 
-NOTIFY_FAILURE = ->(r) { !r[:success] }
+NOTIFY_FAILURE = ->(r) { !r[:success] }.freeze
 
 def run(*args, label:, notify_on: NOTIFY_FAILURE)
   ohai(label)
   print output = Utils.popen_read(*args, err: :out)
 
-  if notify_on.call({success: $CHILD_STATUS.success?, output: output})
-    notify(group: label, subtitle: label, message: output, terminal: args)
-  end
-end
+  return unless notify_on.call({ success: $CHILD_STATUS.success?, output: })
 
+  notify(group: label, subtitle: label, message: output, terminal: args)
+end
 
 puts ""
 safe_system "date"
 
-if `pmset -g batt` =~ /Battery Power/
+if `pmset -g batt`.include?("Battery Power")
   puts "skipping brew unattended-upgrade on battery"
   exit 0
 end
 
 run HOMEBREW_BREW_FILE, "doctor",
-  label: "brew doctor",
-  notify_on: ->(r) { !r[:success] && !OS::Mac.version.prerelease? }
+    label:     "brew doctor",
+    notify_on: ->(r) { !r[:success] && !OS::Mac.version.prerelease? }
 
 run! HOMEBREW_BREW_FILE, "update", label: "brew update"
 run! HOMEBREW_BREW_FILE, "upgrade", label: "brew upgrade"
@@ -84,18 +89,18 @@ run! HOMEBREW_BREW_FILE, "upgrade", label: "brew upgrade"
 
 if bundle_file
   run! HOMEBREW_BREW_FILE, "bundle", "install", "--no-lock", "--file=#{bundle_file}",
-    label: "brew bundle install"
+       label: "brew bundle install"
 end
 
 # Disable bootsnap during cleanup
-ENV['HOMEBREW_NO_BOOTSNAP'] = "1"
+ENV["HOMEBREW_NO_BOOTSNAP"] = "1"
 
 run! HOMEBREW_BREW_FILE, "cleanup", label: "brew cleanup"
 
 if bundle_file
   run HOMEBREW_BREW_FILE, "bundle", "cleanup", "--file=#{bundle_file}",
-    label: "brew bundle cleanup",
-    notify_on: ->(r) { r[:output] != "" }
+      label:     "brew bundle cleanup",
+      notify_on: ->(r) { r[:output] != "" }
 end
 
 ohai "success"
--- itunes-library-export.rb
+++ itunes-library-export.rb
@@ -1,9 +1,9 @@
 class ItunesLibraryExport < Formula
-  desc "A command line tool to export iTunes Library XML files"
+  desc "Command-line tool to export iTunes Library XML files"
   homepage "https://github.com/josh/itunes-library-export"
   url "https://github.com/josh/itunes-library-export/archive/v0.1.1.tar.gz"
   sha256 "9323edcbb0683e45ba4b2b0d4be423ddb3fd903c62535d3c83f9de9ae11b2756"
-  
+
   depends_on xcode: ["12.5", :build]
 
   def install
