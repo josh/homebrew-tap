ed72a9d78676b793145573da7d5774820a685133
Joshua Peek <josh@users.noreply.github.com>
2024-10-30 11:08:30 -0700

    Fix archive url

--- gametrack-data.rb
+++ gametrack-data.rb
@@ -3,7 +3,7 @@ class GametrackData < Formula
 
   desc "Export GameTrack data to CSV"
   homepage "https://github.com/josh/gametrack-data"
-  url "https://github.com/josh/gametrack-data/archive/v1.0.0.tar.gz"
+  url "https://github.com/josh/gametrack-data/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "93f71941575a3f4a367659eafd374977beabc3c14f088cf47bc09edcc0b1ce05"
   license "MIT"
 
