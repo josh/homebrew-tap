977f368765039eeaa818e50b3166513acefe6267
Joshua Peek <josh@users.noreply.github.com>
2025-05-30 14:51:49 -0700

    Update restic-age-key

--- restic-age-key.rb
+++ restic-age-key.rb
@@ -1,9 +1,9 @@
 class ResticAgeKey < Formula
   desc "Use asymmetric age keys instead of a password on your restic repository"
   homepage "https://github.com/josh/restic-age-key"
-  url "https://github.com/josh/restic-age-key/archive/c5c0736997b65859fd58238851675b15afb679db.tar.gz"
-  version "0.0.0"
-  sha256 "f69cb04c28b654b131c85d7601b7166b2d961c380812eb403bf3b0167c406fd2"
+  url "https://github.com/josh/restic-age-key/archive/refs/tags/v0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "330d54f949cd4f05dd7f169c660bfcd880e999633aa9f5bf6ed24a358b43f5ae"
   license "MIT"
 
   depends_on "go" => :build
